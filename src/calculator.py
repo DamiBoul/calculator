7e969268b935f3ce5f3fcd9ab6d2cb33547ca38e
Boulanger Damien <s176671@ic2s210-04.univ-lemans.fr>
2025-03-04 09:44:28 +0100

    ajout fonction 1-Max

--- src/calculator.py
+++ src/calculator.py
@@ -2,3 +2,6 @@ class Calculator :
     # return the sum between 2 operands
     def mysum(self, first_operand, second_operand):
         return first_operand + second_operand
+
+    def myavg(self, first_operand, second_operand):
+        return (first_operand + second_operand)/2
--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from calculator import Calculator
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-op", "--operation", help="(sum, ...)")
+    parser.add_argument("-op", "--operation", help="(sum, avg...)")
     parser.add_argument("-val1", "--first_value", type=int, help="Give the firt value")
     parser.add_argument("-val2", "--second_value", type=int, help="Give the second value")
 
@@ -12,6 +12,8 @@ if __name__ == '__main__':
     calc = Calculator()
 
     if args.operation == 'sum':
-        print(f'{args.first_value} + {args.second_value} = {calc.mysum(args.first_value, args.second_value)}')
+        print(f'{args.first_value} + {args.second_value} = {calc.mysum(args.first_value, args.second_value)}.')
+    elif args.operation == 'avg':
+        print(f'La moyenne de {args.first_value} et {args.second_value} est {calc.myavg(args.first_value, args.second_value)}.')
     else :
         print('The function is not taken into account in our calculator')

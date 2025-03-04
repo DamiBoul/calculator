6c4bd48b563de656ed938111ef87fd88ef2f5c2e
kerryghan-relot <github.stank355@passmail.net>
2025-03-04 09:52:14 +0100

    ajout de la fonctionnalité max

--- src/main.py
+++ src/main.py
@@ -7,11 +7,14 @@ if __name__ == '__main__':
     parser.add_argument("-op", "--operation", help="(sum, ...)")
     parser.add_argument("-val1", "--first_value", type=int, help="Give the firt value")
     parser.add_argument("-val2", "--second_value", type=int, help="Give the second value")
+    parser.add_argument("-val3", "--second_value", type=int, help="Give the third value", nargs='?', default=None)
 
     args = parser.parse_args()
     calc = Calculator()
 
     if args.operation == 'sum':
         print(f'{args.first_value} + {args.second_value} = {calc.mysum(args.first_value, args.second_value)}')
+    elif args.operation == 'max':
+        print(f'The max between {args.first_value}, {args.second_value}, {args.third_value} is {calc.mymax(args.first_value, args.second_value, args.third_value)}')
     else :
         print('The function is not taken into account in our calculator')

4c83e1955d15f1256538292508d5febe58cd6584
kerryghan-relot <github.stank355@passmail.net>
2025-03-04 09:57:58 +0100

    fix commentaire damien

--- src/main.py
+++ src/main.py
@@ -4,10 +4,10 @@ from calculator import Calculator
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-op", "--operation", help="(sum, ...)")
+    parser.add_argument("-op", "--operation", help="(sum, max,...)")
     parser.add_argument("-val1", "--first_value", type=int, help="Give the firt value")
     parser.add_argument("-val2", "--second_value", type=int, help="Give the second value")
-    parser.add_argument("-val3", "--second_value", type=int, help="Give the third value", nargs='?', default=None)
+    parser.add_argument("-val3", "--third_value", type=int, help="Give the third value", nargs='?', default=None)
 
     args = parser.parse_args()
     calc = Calculator()
